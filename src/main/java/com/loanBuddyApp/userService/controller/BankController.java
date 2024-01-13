d998fac0d6c7f233015cdd4226fab5c631a8e62b
Bharath Raam <bharathsrini19@gmail.com>
2024-01-13 15:57:41 -0500

    Adding Explicit cors on bank controller | Bug Cors test fix 2

--- src/main/java/com/loanBuddyApp/userService/controller/BankController.java
+++ src/main/java/com/loanBuddyApp/userService/controller/BankController.java
@@ -4,7 +4,6 @@ import com.loanBuddyApp.userService.dto.TransactionRequest;
 import com.loanBuddyApp.userService.model.Transaction;
 import com.loanBuddyApp.userService.service.BankService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +16,7 @@ public class BankController {
 
     private final BankService bankService;
     @PostMapping("/transfer")
-    public ResponseEntity<?> transferMoney(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody TransactionRequest transactionRequest) throws Exception {
+    public ResponseEntity<?> transferMoney(@RequestHeader("Authorization") String authorizationHeader, @RequestBody TransactionRequest transactionRequest) throws Exception {
         try{
             Transaction transaction =bankService.transferMoney(transactionRequest,authorizationHeader);
             return ResponseEntity.ok(transaction);
