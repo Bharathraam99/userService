7dcb304e5cf723dbd350b7e0daca1fa0a5a953c3
Bharath Raam <bharathsrini19@gmail.com>
2024-01-16 11:14:27 -0500

    Final bug fix cors bank || Bug Fix

--- src/main/java/com/loanBuddyApp/userService/config/SecurityConfiguration.java
+++ src/main/java/com/loanBuddyApp/userService/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/v1/user").hasAnyAuthority(Role.USER.name())
                         .requestMatchers("/api/v1/manager").hasAnyAuthority(Role.MANAGER.name())
                         .requestMatchers("/api/v1/bank").hasAnyAuthority(Role.USER.name())
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
--- src/main/java/com/loanBuddyApp/userService/controller/BankController.java
+++ src/main/java/com/loanBuddyApp/userService/controller/BankController.java
@@ -4,19 +4,19 @@ import com.loanBuddyApp.userService.dto.TransactionRequest;
 import com.loanBuddyApp.userService.model.Transaction;
 import com.loanBuddyApp.userService.service.BankService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/bank")
 @RequiredArgsConstructor
 public class BankController {
 
     private final BankService bankService;
     @PostMapping("/transfer")
-    public ResponseEntity<?> transferMoney(@RequestHeader("Authorization") String authorizationHeader, @RequestBody TransactionRequest transactionRequest) throws Exception {
+    public ResponseEntity<?> transferMoney(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody TransactionRequest transactionRequest) throws Exception {
         try{
             Transaction transaction =bankService.transferMoney(transactionRequest,authorizationHeader);
             return ResponseEntity.ok(transaction);
